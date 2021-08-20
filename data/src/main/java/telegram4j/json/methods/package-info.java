340bc5fb60bcaaacb86a5e343189435d141d9c88
skat <fishmoders@petruchio.org.ru>
2021-08-20 19:15:46 +0300

    Replace GetMeRequest to Dispatch object

--- data/src/main/java/telegram4j/json/methods/package-info.java
+++
@@ -1,9 +0,0 @@
-@Value.Style(
-        depluralize = true,
-        jdkOnly = true,
-        allParameters = true,
-        defaultAsDefault = true
-)
-package telegram4j.json.methods;
-
-import org.immutables.value.Value;
