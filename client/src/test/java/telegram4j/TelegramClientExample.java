167f0e130e632d36cc4a72cdca0e65efeb646962
skat <fishmoders@petruchio.org.ru>
2021-08-21 01:47:07 +0300

    Upload client initialize example

--- client/src/test/java/telegram4j/TelegramClientExample.java
+++ client/src/test/java/telegram4j/TelegramClientExample.java
@@ -4,6 +4,9 @@ public class TelegramClientExample {
 
     public static void main(String[] args) {
         TelegramClient telegramClient = TelegramClient.create(System.getenv("T4J_TOKEN"));
-        // TODO
+
+        telegramClient.getRestClient().getApplicationService().getUpdates().log().subscribe();
+
+        while (true);
     }
 }
