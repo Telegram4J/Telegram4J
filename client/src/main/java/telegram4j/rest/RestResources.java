34cfc9a4dfe13b7882517f9553aa44f70522f5c8
skat <fishmoders@petruchio.org.ru>
2021-08-20 19:13:59 +0300

    Make json deserializing compatible with lower case enum

--- client/src/main/java/telegram4j/rest/RestResources.java
+++ client/src/main/java/telegram4j/rest/RestResources.java
@@ -1,6 +1,8 @@
 package telegram4j.rest;
 
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import reactor.netty.http.client.HttpClient;
 
@@ -10,8 +12,10 @@ import java.util.function.Supplier;
 public class RestResources {
 
     public static final Supplier<HttpClient> DEFAULT_HTTP_CLIENT = () -> HttpClient.create().compress(true);
-    public static final Supplier<ObjectMapper> DEFAULT_OBJECT_MAPPER = () -> new ObjectMapper()
-            .registerModule(new Jdk8Module());
+    public static final Supplier<ObjectMapper> DEFAULT_OBJECT_MAPPER = () -> JsonMapper.builder()
+            .addModules(new Jdk8Module())
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .build();
 
     private final HttpClient httpClient;
     private final ObjectMapper objectMapper;
