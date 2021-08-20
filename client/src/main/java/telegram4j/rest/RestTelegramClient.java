5e6849dcfa3ff5010220f35abad9f4ee6b79717d
skat <fishmoders@petruchio.org.ru>
2021-08-21 01:38:50 +0300

    Add application service

--- client/src/main/java/telegram4j/rest/RestTelegramClient.java
+++ client/src/main/java/telegram4j/rest/RestTelegramClient.java
@@ -1,16 +1,23 @@
 package telegram4j.rest;
 
+import telegram4j.rest.service.ApplicationService;
 import telegram4j.rest.service.ChatService;
 
 public final class RestTelegramClient {
 
+    private final ApplicationService applicationService;
     private final ChatService chatService;
 
     public RestTelegramClient(RestRouter restRouter) {
         this.chatService = new ChatService(restRouter);
+        this.applicationService = new ApplicationService(restRouter);
     }
 
     public ChatService getChatService() {
         return chatService;
+    }
+
+    public ApplicationService getApplicationService() {
+        return applicationService;
     }
 }
