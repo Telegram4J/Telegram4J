12fb9e30d71edeb695c4a71bae209010abb89a5c
summetdev <summet.dev@gmail.com>
2021-08-20 22:22:21 +0300

    ChatMember serialization
    See https://core.telegram.org/bots/api#chatmember

--- data/src/main/java/telegram4j/json/BotCommandScopeData.java
+++ data/src/main/java/telegram4j/json/BotCommandScopeData.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
 public interface BotCommandScopeData {
+
     BotCommandScopeType type();
 
     @Value.Immutable
