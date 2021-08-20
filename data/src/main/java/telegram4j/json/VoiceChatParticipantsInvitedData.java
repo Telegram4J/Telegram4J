fd39020d2e53c66fb425443263c02b6ff42333f6
summetdev <summet.dev@gmail.com>
2021-08-20 19:30:08 +0300

    Added missing `singleton = true` parameter

--- data/src/main/java/telegram4j/json/VoiceChatParticipantsInvitedData.java
+++ data/src/main/java/telegram4j/json/VoiceChatParticipantsInvitedData.java
@@ -7,7 +7,7 @@ import org.immutables.value.Value;
 import java.util.List;
 import java.util.Optional;
 
-@Value.Immutable
+@Value.Immutable(singleton = true) // because all fields are optional
 @JsonSerialize(as = ImmutableVoiceChatParticipantsInvitedData.class)
 @JsonDeserialize(as = ImmutableVoiceChatParticipantsInvitedData.class)
 public interface VoiceChatParticipantsInvitedData {
