2fb119c8ce482898635d8dcefa0f30a27b147a14
skat <fishmoders@petruchio.org.ru>
2021-08-21 05:38:59 +0300

    Collapse polymorphic types to one class

--- data/src/main/java/telegram4j/json/ChatMemberData.java
+++ data/src/main/java/telegram4j/json/ChatMemberData.java
@@ -1,193 +1,79 @@
 package telegram4j.json;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeId;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
 import java.util.Optional;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "status")
-@JsonSubTypes({
-        @JsonSubTypes.Type(name = "owner", value = ChatMemberData.ChatMemberOwnerData.class),
-        @JsonSubTypes.Type(name = "administrator", value = ChatMemberData.ChatMemberAdministratorData.class),
-        @JsonSubTypes.Type(name = "member", value = ChatMemberData.ChatMemberMemberData.class),
-        @JsonSubTypes.Type(name = "restricted", value = ChatMemberData.ChatMemberRestrictedData.class),
-        @JsonSubTypes.Type(name = "left", value = ChatMemberData.ChatMemberLeftData.class),
-        @JsonSubTypes.Type(name = "banned", value = ChatMemberData.ChatMemberBannedData.class)
-})
+@Value.Immutable
+@JsonSerialize(as = ImmutableChatMemberData.class)
+@JsonDeserialize(as = ImmutableChatMemberData.class)
 public interface ChatMemberData {
 
-    @JsonTypeId
+    static ImmutableChatMemberData.Builder builder() {
+        return ImmutableChatMemberData.builder();
+    }
+
     ChatMemberType status();
 
     UserData user();
 
-    @Value.Immutable
-    @JsonSerialize(as = ImmutableChatMemberOwnerData.class)
-    @JsonDeserialize(as = ImmutableChatMemberOwnerData.class)
-    interface ChatMemberOwnerData extends ChatMemberData {
+    @JsonProperty("is_anonymous")
+    Optional<Boolean> isAnonymous();
 
-        static ImmutableChatMemberOwnerData.Builder builder() {
-            return ImmutableChatMemberOwnerData.builder();
-        }
+    @JsonProperty("custom_title")
+    Optional<String> customTitle();
 
-        @Override
-        default ChatMemberType status() {
-            return ChatMemberType.OWNER;
-        }
+    @JsonProperty("can_be_edited")
+    Optional<Boolean> canBeEdited();
 
-        @JsonProperty("is_anonymous")
-        boolean isAnonymous();
+    @JsonProperty("can_manage_chat")
+    Optional<Boolean> canManageChat();
 
-        @JsonProperty("custom_title")
-        Optional<String> customTitle();
-    }
+    @JsonProperty("can_delete_messages")
+    Optional<Boolean> canDeleteMessages();
 
-    @Value.Immutable
-    @JsonSerialize(as = ImmutableChatMemberAdministratorData.class)
-    @JsonDeserialize(as = ImmutableChatMemberAdministratorData.class)
-    interface ChatMemberAdministratorData extends ChatMemberData {
+    @JsonProperty("can_manage_voice_chats")
+    Optional<Boolean> canManageVoiceChats();
 
-        static ImmutableChatMemberAdministratorData.Builder builder() {
-            return ImmutableChatMemberAdministratorData.builder();
-        }
+    @JsonProperty("can_restrict_members")
+    Optional<Boolean> canRestrictMembers();
 
-        @Override
-        default ChatMemberType status() {
-            return ChatMemberType.ADMINISTRATOR;
-        }
+    @JsonProperty("can_promote_members")
+    Optional<Boolean> canPromoteMembers();
 
-        @JsonProperty("can_be_edited")
-        boolean canBeEdited();
+    @JsonProperty("can_change_info")
+    Optional<Boolean> canChangeInfo();
 
-        @JsonProperty("is_anonymous")
-        boolean isAnonymous();
+    @JsonProperty("can_invite_users")
+    Optional<Boolean> canInviteUsers();
 
-        @JsonProperty("can_manage_chat")
-        boolean canManageChat();
+    @JsonProperty("can_post_messages")
+    Optional<Boolean> canPostMessages();
 
-        @JsonProperty("can_delete_messages")
-        boolean canDeleteMessages();
+    @JsonProperty("can_edit_messages")
+    Optional<Boolean> canEditMessages();
 
-        @JsonProperty("can_manage_voice_chats")
-        boolean canManageVoiceChats();
+    @JsonProperty("can_pin_messages")
+    Optional<Boolean> canPinMessages();
 
-        @JsonProperty("can_restrict_members")
-        boolean canRestrictMembers();
+    @JsonProperty("is_member")
+    Optional<Boolean> isMember();
 
-        @JsonProperty("can_promote_members")
-        boolean canPromoteMembers();
+    @JsonProperty("can_send_media_messages")
+    Optional<Boolean> canSendMediaMessages();
 
-        @JsonProperty("can_change_info")
-        boolean canChangeInfo();
+    @JsonProperty("can_send_polls")
+    Optional<Boolean> canSendPolls();
 
-        @JsonProperty("can_invite_users")
-        boolean canInviteUsers();
+    @JsonProperty("can_send_other_messages")
+    Optional<Boolean> canSendOtherMessages();
 
-        @JsonProperty("can_post_messages")
-        Optional<Boolean> canPostMessages();
+    @JsonProperty("can_add_web_page_previews")
+    Optional<Boolean> canAddWebPagePreviews();
 
-        @JsonProperty("can_edit_messages")
-        Optional<Boolean> canEditMessages();
-
-        @JsonProperty("can_pin_messages")
-        Optional<Boolean> canPinMessages();
-
-        @JsonProperty("custom_title")
-        Optional<String> customTitle();
-    }
-
-    @Value.Immutable
-    @JsonSerialize(as = ImmutableChatMemberMemberData.class)
-    @JsonDeserialize(as = ImmutableChatMemberMemberData.class)
-    interface ChatMemberMemberData extends ChatMemberData {
-
-        static ImmutableChatMemberMemberData.Builder builder() {
-            return ImmutableChatMemberMemberData.builder();
-        }
-
-        @Override
-        default ChatMemberType status() {
-            return ChatMemberType.MEMBER;
-        }
-    }
-
-    @Value.Immutable
-    @JsonSerialize(as = ImmutableChatMemberRestrictedData.class)
-    @JsonDeserialize(as = ImmutableChatMemberRestrictedData.class)
-    interface ChatMemberRestrictedData extends ChatMemberData {
-
-        static ImmutableChatMemberRestrictedData.Builder builder() {
-            return ImmutableChatMemberRestrictedData.builder();
-        }
-
-        @Override
-        default ChatMemberType status() {
-            return ChatMemberType.RESTRICTED;
-        }
-
-        @JsonProperty("is_member")
-        boolean isMember();
-
-        @JsonProperty("can_change_info")
-        boolean canChangeInfo();
-
-        @JsonProperty("can_invite_users")
-        boolean canInviteUsers();
-
-        @JsonProperty("can_pin_messages")
-        boolean canPinMessages();
-
-        @JsonProperty("can_send_media_messages")
-        boolean canSendMediaMessages();
-
-        @JsonProperty("can_send_polls")
-        boolean canSendPolls();
-
-        @JsonProperty("can_send_other_messages")
-        boolean canSendOtherMessages();
-
-        @JsonProperty("can_add_web_page_previews")
-        boolean canAddWebPagePreviews();
-
-        @JsonProperty("until_date")
-        int untilDate();
-    }
-
-    @Value.Immutable
-    @JsonSerialize(as = ImmutableChatMemberLeftData.class)
-    @JsonDeserialize(as = ImmutableChatMemberLeftData.class)
-    interface ChatMemberLeftData extends ChatMemberData {
-
-        static ImmutableChatMemberLeftData.Builder builder() {
-            return ImmutableChatMemberLeftData.builder();
-        }
-
-        @Override
-        default ChatMemberType status() {
-            return ChatMemberType.LEFT;
-        }
-    }
-
-    @Value.Immutable
-    @JsonSerialize(as = ImmutableChatMemberBannedData.class)
-    @JsonDeserialize(as = ImmutableChatMemberBannedData.class)
-    interface ChatMemberBannedData extends ChatMemberData {
-
-        static ImmutableChatMemberBannedData.Builder builder() {
-            return ImmutableChatMemberBannedData.builder();
-        }
-
-        @Override
-        default ChatMemberType status() {
-            return ChatMemberType.BANNED;
-        }
-
-        @JsonProperty("until_date")
-        int untilDate();
-    }
+    @JsonProperty("until_date")
+    Optional<Integer> untilDate();
 }
--- data/src/main/java/telegram4j/json/InputMediaData.java
+++ data/src/main/java/telegram4j/json/InputMediaData.java
@@ -8,164 +8,42 @@ import org.immutables.value.Value;
 import java.util.List;
 import java.util.Optional;
 
+@Value.Immutable
+@JsonSerialize(as = ImmutableInputMediaData.class)
+@JsonDeserialize(as = ImmutableInputMediaData.class)
 public interface InputMediaData {
+
+    static ImmutableInputMediaData.Builder builder() {
+        return ImmutableInputMediaData.builder();
+    }
 
     InputMediaType type();
 
-    @Value.Immutable
-    @JsonSerialize(as = ImmutableInputMediaPhotoData.class)
-    @JsonDeserialize(as = ImmutableInputMediaPhotoData.class)
-    interface InputMediaPhotoData extends InputMediaData {
+    Optional<String> media();
 
-        static ImmutableInputMediaPhotoData.Builder builder() {
-            return ImmutableInputMediaPhotoData.builder();
-        }
+    Optional<String> caption();
 
-        @Override
-        default InputMediaType type() {
-            return InputMediaType.PHOTO;
-        }
+    @JsonProperty("parse_mode")
+    Optional<String> parseMode();
 
-        String media();
+    @JsonProperty("caption_entities")
+    Optional<List<MessageEntityData>> captionEntities();
 
-        Optional<String> caption();
+    Optional<String> thumb();
 
-        @JsonProperty("parse_mode")
-        Optional<String> parseMode();
+    Optional<Integer> width();
 
-        @JsonProperty("caption_entities")
-        Optional<List<MessageEntityData>> captionEntities();
-    }
+    Optional<Integer> height();
 
-    @Value.Immutable
-    @JsonSerialize(as = ImmutableInputMediaVideoData.class)
-    @JsonDeserialize(as = ImmutableInputMediaVideoData.class)
-    interface InputMediaVideoData extends InputMediaData {
+    Optional<Integer> duration();
 
-        static ImmutableInputMediaVideoData.Builder builder() {
-            return ImmutableInputMediaVideoData.builder();
-        }
+    @JsonProperty("supports_streaming")
+    Optional<Boolean> supportsStreaming();
 
-        @Override
-        default InputMediaType type() {
-            return InputMediaType.VIDEO;
-        }
+    Optional<String> performer();
 
-        String media();
+    Optional<String> title();
 
-        Optional<String> thumb();
-
-        Optional<String> caption();
-
-        @JsonProperty("parse_mode")
-        Optional<String> parseMode();
-
-        @JsonProperty("caption_entities")
-        Optional<List<MessageEntityData>> captionEntities();
-
-        Optional<Integer> width();
-
-        Optional<Integer> height();
-
-        Optional<Integer> duration();
-
-        @JsonProperty("supports_streaming")
-        Optional<Boolean> supportsStreaming();
-    }
-
-    @Value.Immutable
-    @JsonSerialize(as = ImmutableInputMediaAnimationData.class)
-    @JsonDeserialize(as = ImmutableInputMediaAnimationData.class)
-    interface InputMediaAnimationData extends InputMediaData {
-
-        static ImmutableInputMediaAnimationData.Builder builder() {
-            return ImmutableInputMediaAnimationData.builder();
-        }
-
-        @Override
-        default InputMediaType type() {
-            return InputMediaType.ANIMATION;
-        }
-
-        String media();
-
-        Optional<String> thumb();
-
-        Optional<String> caption();
-
-        @JsonProperty("parse_mode")
-        Optional<String> parseMode();
-
-        @JsonProperty("caption_entities")
-        Optional<List<MessageEntityData>> captionEntities();
-
-        Optional<Integer> width();
-
-        Optional<Integer> height();
-
-        Optional<Integer> duration();
-    }
-
-    @Value.Immutable
-    @JsonSerialize(as = ImmutableInputMediaAudioData.class)
-    @JsonDeserialize(as = ImmutableInputMediaAudioData.class)
-    interface InputMediaAudioData extends InputMediaData {
-
-        static ImmutableInputMediaAudioData.Builder builder() {
-            return ImmutableInputMediaAudioData.builder();
-        }
-
-        @Override
-        default InputMediaType type() {
-            return InputMediaType.AUDIO;
-        }
-
-        String media();
-
-        Optional<String> thumb();
-
-        Optional<String> caption();
-
-        @JsonProperty("parse_mode")
-        Optional<String> parseMode();
-
-        @JsonProperty("caption_entities")
-        Optional<List<MessageEntityData>> captionEntities();
-
-        Optional<Integer> duration();
-
-        Optional<String> performer();
-
-        Optional<String> title();
-    }
-
-    @Value.Immutable
-    @JsonSerialize(as = ImmutableInputMediaDocumentData.class)
-    @JsonDeserialize(as = ImmutableInputMediaDocumentData.class)
-    interface InputMediaDocumentData extends InputMediaData {
-
-        static ImmutableInputMediaDocumentData.Builder builder() {
-            return ImmutableInputMediaDocumentData.builder();
-        }
-
-        @Override
-        default InputMediaType type() {
-            return InputMediaType.DOCUMENT;
-        }
-
-        String media();
-
-        Optional<String> thumb();
-
-        Optional<String> caption();
-
-        @JsonProperty("parse_mode")
-        Optional<String> parseMode();
-
-        @JsonProperty("caption_entities")
-        Optional<List<MessageEntityData>> captionEntities();
-
-        @JsonProperty("disable_content_type_detection")
-        Optional<Boolean> disableContentTypeDetection();
-    }
+    @JsonProperty("disable_content_type_detection")
+    Optional<Boolean> disableContentTypeDetection();
 }
