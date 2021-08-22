e97cca3b570d13cd7a2e726e6deef0acb6072dfb
skat <fishmoders@petruchio.org.ru>
2021-08-22 11:08:51 +0300

    Implement updates fetching, event mapping

--- core/src/main/java/telegram4j/core/TelegramClient.java
+++ core/src/main/java/telegram4j/core/TelegramClient.java
@@ -1,10 +1,19 @@
 package telegram4j.core;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+import reactor.util.concurrent.Queues;
+import telegram4j.core.dispatch.UpdateContext;
+import telegram4j.core.event.Event;
+import telegram4j.json.UpdateData;
 import telegram4j.rest.DefaultRouter;
 import telegram4j.rest.RestResources;
 import telegram4j.rest.RestTelegramClient;
 import telegram4j.rest.RouterResources;
+
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class TelegramClient {
 
@@ -13,12 +22,21 @@ public final class TelegramClient {
     private final RestTelegramClient restClient;
     private final ClientResources clientResources;
 
+    private final AtomicInteger lastUpdateId = new AtomicInteger();
+    private final Sinks.Many<UpdateData> updates;
+
     TelegramClient(String token, RestResources restResources, ClientResources clientResources) {
         this.token = token;
         this.restResources = restResources;
         this.restClient = new RestTelegramClient(new DefaultRouter(
                 new RouterResources(token, restResources)));
         this.clientResources = clientResources;
+
+        this.updates = newManySink();
+    }
+
+    private static <T> Sinks.Many<T> newManySink() {
+        return Sinks.many().multicast().onBackpressureBuffer(Queues.SMALL_BUFFER_SIZE, false);
     }
 
     public static TelegramClient create(String token) {
@@ -37,8 +55,34 @@ public final class TelegramClient {
         return restClient;
     }
 
+    public ClientResources getClientResources() {
+        return clientResources;
+    }
+
+    public <E extends Event> Flux<E> on(Class<E> type) {
+        return clientResources.getEventDispatcher().on(type);
+    }
+
     public Mono<Void> login() {
-        // Currently nothing
-        return Mono.empty();
+        return Mono.defer(() -> {
+
+            Mono<Void> readUpdates = Flux.interval(clientResources.getUpdateInterval())
+                    .flatMap(l -> getRestClient().getApplicationService().getUpdates()
+                            .filter(data -> data.updateId() > lastUpdateId.get())
+                            .sort(Comparator.comparingInt(UpdateData::updateId))
+                            .doOnNext(data -> lastUpdateId.set(data.updateId()))
+                            .checkpoint("Read updates from API")
+                            .doOnNext(updates::tryEmitNext))
+                    .then();
+
+            Mono<Void> mapEvents = updates.asFlux()
+                    .flatMap(data -> clientResources.getDispatchMapper().<Event>handle(
+                            new UpdateContext(data, this)))
+                    .checkpoint("Map updates to events")
+                    .doOnNext(clientResources.getEventDispatcher()::publish)
+                    .then();
+
+            return Mono.when(readUpdates, mapEvents);
+        });
     }
 }
