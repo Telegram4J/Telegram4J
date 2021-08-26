a4490d980473247196b0a73b20e7371a030d2d16
Misaka <misakachan2545@gmail.com>
2021-08-27 00:28:24 +0300

    JavaDocs for TelegramClient and TelegramClientBuilder

--- core/src/main/java/telegram4j/core/TelegramClient.java
+++ core/src/main/java/telegram4j/core/TelegramClient.java
@@ -3,26 +3,54 @@ package telegram4j.core;
 import reactor.core.publisher.Mono;
 import telegram4j.rest.RestResources;
 
-public final class TelegramClient {
 
+/**
+ * Base class for access to library functions
+ */
+public final class TelegramClient {
+    /**Bot secret key, issued by a BotFather(Telegram bot for create bot's accounts) when creating new bot*/
     private final String token;
+    /**Access to rest content*/
     private final RestResources restResources;
+    /**Access to events and updates handling*/
     private final ClientResources clientResources;
 
+    /**
+     * Constructor for this class. Not recommended for use. For create class instance use TelegramClientBuilder
+     * @see TelegramClientBuilder
+     * @param token access token
+     * @param restResources REST instance
+     * @param clientResources Events and Updates handling instance
+     */
     TelegramClient(String token, RestResources restResources, ClientResources clientResources) {
         this.token = token;
         this.restResources = restResources;
         this.clientResources = clientResources;
     }
 
+    /**
+     * Fast create of TelegramClient instance
+     * @param token bot access token
+     * @return ready to login class instance
+     *
+     * @see TelegramClient#login()
+     */
     public static TelegramClient create(String token) {
         return builder().setToken(token).build();
     }
 
+    /**
+     * @see TelegramClientBuilder
+     * @return new {@link TelegramClientBuilder}
+     */
     public static TelegramClientBuilder builder() {
         return new TelegramClientBuilder();
     }
 
+    /**
+     * Method for auth bot
+     * @return nothing or error for handle
+     */
     public Mono<Void> login() {
         // Currently nothing
         return Mono.empty();
--- core/src/main/java/telegram4j/core/TelegramClientBuilder.java
+++ core/src/main/java/telegram4j/core/TelegramClientBuilder.java
@@ -6,33 +6,59 @@ import java.util.Objects;
 
 public class TelegramClientBuilder {
 
+    /**Bot secret key, issued by a BotFather(Telegram bot for create bot's accounts) when creating new bot*/
     private String token;
+    /**Access to rest content*/
     private RestResources restResources;
+    /**Access to events and updates handling*/
     private ClientResources clientResources;
 
     TelegramClientBuilder() {}
 
+    /**
+     * Token setter
+     * @param token Bot secret key. Cannot be null
+     * @return updated object with defined token
+     */
     public TelegramClientBuilder setToken(String token) {
         this.token = Objects.requireNonNull(token, "token");
         return this;
     }
 
+    /**
+     * Rest resources setter
+     * @param restResources rest resources. Cannot be null
+     * @return updated object with defined rest
+     */
     public TelegramClientBuilder setRestResources(RestResources restResources) {
         this.restResources = Objects.requireNonNull(restResources, "restResources");
         return this;
     }
 
+    /**
+     * Client resources setter
+     * @param clientResources client resources. Cannot be null
+     * @return
+     */
     public TelegramClientBuilder setClientResources(ClientResources clientResources) {
         this.clientResources = Objects.requireNonNull(clientResources, "clientResources");
         return this;
     }
 
+    /**
+     * Method for build a bot. make sure that you have set the token(for set use {@link TelegramClientBuilder#setToken(String)}
+     * @return {@link TelegramClient} ready to login
+     */
     public TelegramClient build() {
         RestResources restResources = getRestResources();
         ClientResources clientResources = getClientResources();
         return new TelegramClient(token, restResources, clientResources);
     }
 
+    /**
+     * private method for get REST resources
+     * @return if {@link TelegramClientBuilder#restResources} is null return new instance, else return value of {@link TelegramClientBuilder#restResources}
+     */
     private RestResources getRestResources() {
         if (restResources != null) {
             return restResources;
@@ -40,6 +66,10 @@ public class TelegramClientBuilder {
         return new RestResources();
     }
 
+    /**
+     * private method for get Client resources
+     * @return if {@link TelegramClientBuilder#clientResources} is null return new instance, else return value of {@link TelegramClientBuilder#clientResources}
+     */
     private ClientResources getClientResources() {
         if (clientResources != null) {
             return clientResources;
