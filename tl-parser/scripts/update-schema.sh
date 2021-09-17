7a03fbe0b748a453f1961b1d54837b5392accfd3
skat <fishmoders@petruchio.org.ru>
2021-09-17 17:16:44 +0300

    Add mtproto json scheme

--- tl-parser/scripts/update-schema.sh
+++ tl-parser/scripts/update-schema.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 curl -s 'https://core.telegram.org/schema/json' | jq . --indent 2 > ../src/main/resources/api.json
+curl -s 'https://core.telegram.org/schema/mtproto-json' | jq . --indent 2 > ../src/main/resources/mtproto.json
