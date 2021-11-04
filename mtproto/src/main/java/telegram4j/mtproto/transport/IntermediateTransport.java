aafde5f9e74875ceda6b4caf70528dc632795cfa
skat <fishmoders@petruchio.org.ru>
2021-11-04 13:16:03 +0300

    Create IntermediateTransport

---
+++ mtproto/src/main/java/telegram4j/mtproto/transport/IntermediateTransport.java
@@ -0,0 +1,24 @@
+package telegram4j.mtproto.transport;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+
+public class IntermediateTransport implements Transport {
+    @Override
+    public ByteBuf identifier(ByteBufAllocator allocator) {
+        return allocator.buffer(Integer.BYTES).writeIntLE(0xeeeeeeee);
+    }
+
+    @Override
+    public ByteBuf encode(ByteBufAllocator allocator, ByteBuf payload) {
+        return allocator.buffer(Integer.BYTES + payload.writerIndex())
+                .writeIntLE(payload.writerIndex())
+                .writeBytes(payload);
+    }
+
+    @Override
+    public ByteBuf decode(ByteBufAllocator allocator, ByteBuf payload) {
+        int length = payload.readIntLE();
+        return payload.readBytes(length);
+    }
+}
