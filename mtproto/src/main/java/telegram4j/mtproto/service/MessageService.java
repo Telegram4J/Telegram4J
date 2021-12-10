65245bc717e6977cecc1b76b7d785c6dd072f3e0
skat <fishmoders@petruchio.org.ru>
2021-12-10 22:32:41 +0300

    Upload base message service

---
+++ mtproto/src/main/java/telegram4j/mtproto/service/MessageService.java
@@ -0,0 +1,227 @@
+package telegram4j.mtproto.service;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+import reactor.function.TupleUtils;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+import telegram4j.mtproto.MTProtoClient;
+import telegram4j.mtproto.store.StoreLayout;
+import telegram4j.mtproto.util.CryptoUtil;
+import telegram4j.tl.*;
+import telegram4j.tl.contacts.ResolvedPeer;
+import telegram4j.tl.request.contacts.ImmutableResolveUsername;
+import telegram4j.tl.request.messages.*;
+import telegram4j.tl.request.upload.SaveFilePart;
+
+import java.security.MessageDigest;
+
+import static telegram4j.mtproto.service.ServiceUtil.mapNullable;
+
+public class MessageService extends RpcService {
+
+    private static final int PART_SIZE = 512 * 1024;
+    private static final int TEN_MB = 10 * 1024 * 1024;
+    private static final int LIMIT_MB = 2000 * 1024 * 1024;
+    private static final int PARALLELISM = 3;
+
+    public MessageService(MTProtoClient client, StoreLayout storeLayout) {
+        super(client, storeLayout);
+    }
+
+    public Mono<ResolvedPeer> resolveUsername(String username) {
+        return Mono.defer(() -> {
+            String corrected = username.toLowerCase()
+                    .trim().replace(".", "");
+
+            if (corrected.startsWith("@")) {
+                corrected = corrected.substring(1);
+            }
+
+            return client.sendAwait(ImmutableResolveUsername.of(corrected));
+        });
+    }
+
+    public Mono<InputFile> saveFile(ByteBuf data, String name) {
+        long fileId = CryptoUtil.random.nextLong();
+        int parts = (int) Math.ceil((float) data.readableBytes() / PART_SIZE);
+        boolean big = data.readableBytes() > TEN_MB;
+
+        if (data.readableBytes() > LIMIT_MB) {
+            return Mono.error(new IllegalArgumentException("File size is under limit. Size: "
+                    + data.readableBytes() + ", limit: " + LIMIT_MB));
+        }
+
+        if (big) {
+
+            return Mono.empty();
+        }
+
+        MessageDigest md5 = CryptoUtil.MD5.get();
+
+        return Flux.range(0, parts)
+                .flatMap(filePart -> {
+                    ByteBuf part = data.readBytes(Math.min(PART_SIZE, data.readableBytes()));
+                    byte[] partBytes = CryptoUtil.toByteArray(part);
+
+                    synchronized (md5) {
+                        md5.update(partBytes);
+                    }
+
+                    SaveFilePart req = SaveFilePart.builder()
+                            .fileId(fileId)
+                            .filePart(filePart)
+                            .bytes(partBytes)
+                            .build();
+
+                    return client.sendAwait(req);
+                })
+                .then(Mono.fromSupplier(() -> ImmutableBaseInputFile.of(fileId,
+                        parts, name, ByteBufUtil.hexDump(md5.digest()))));
+    }
+
+    public Mono<BaseMessage> sendMessage(SendMessage request) {
+        return client.sendAwait(request)
+                .zipWith(toPeer(request.peer()))
+                .map(TupleUtils.function((updates, peer) -> {
+                    Tuple2<BaseMessage, Updates> upd = transformMessageUpdate(request, updates, peer);
+
+                    client.updates().emitNext(upd.getT2(), Sinks.EmitFailureHandler.FAIL_FAST);
+
+                    return upd.getT1();
+                }));
+    }
+
+    public Mono<BaseMessage> sendMedia(SendMedia request) {
+        return client.sendAwait(request)
+                .zipWith(toPeer(request.peer()))
+                .map(TupleUtils.function((updates, peer) -> {
+                    Tuple2<BaseMessage, Updates> upd = transformMessageUpdate(request, updates, peer);
+
+                    client.updates().emitNext(upd.getT2(), Sinks.EmitFailureHandler.FAIL_FAST);
+
+                    return upd.getT1();
+                }));
+    }
+
+    // Short-send related updates object should be transformed to the updateShort.
+    // https://core.telegram.org/api/updates#updates-sequence
+    protected static Tuple2<BaseMessage, Updates> transformMessageUpdate(BaseSendMessageRequest request, Updates updates, Peer peer) {
+        BaseMessage message;
+        Updates update;
+        if (updates instanceof UpdateShortSentMessage) {
+            UpdateShortSentMessage updates0 = (UpdateShortSentMessage) updates;
+            message = BaseMessage.builder()
+                    .silent(request.silent())
+                    .peerId(peer)
+                    .replyTo(mapNullable(request.replyToMsgId(), ImmutableMessageReplyHeader::of))
+                    .message(request.message())
+                    .id(updates0.id())
+                    .replyMarkup(request.replyMarkup())
+                    .out(updates0.out())
+                    .fromScheduled(request.scheduleDate() != null)
+                    .media(updates0.media())
+                    .entities(updates0.entities())
+                    .date(updates0.date())
+                    .ttlPeriod(updates0.ttlPeriod())
+                    .build();
+
+            update = ImmutableUpdateShort.builder()
+                    .date(updates0.date())
+                    .update(UpdateNewMessage.builder()
+                            .message(message)
+                            .pts(updates0.pts())
+                            .ptsCount(updates0.ptsCount())
+                            .build())
+                    .build();
+        } else if (updates instanceof UpdateShortMessage) {
+            UpdateShortMessage updates0 = (UpdateShortMessage) updates;
+
+            message = BaseMessage.builder()
+                    .silent(request.silent())
+                    .peerId(peer)
+                    .replyTo(updates0.replyTo())
+                    .message(request.message())
+                    .id(updates0.id())
+                    .replyMarkup(request.replyMarkup())
+                    .out(updates0.out())
+                    .fromScheduled(request.scheduleDate() != null)
+                    .fwdFrom(updates0.fwdFrom())
+                    .entities(updates0.entities())
+                    .date(updates0.date())
+                    .viaBotId(updates0.viaBotId())
+                    .ttlPeriod(updates0.ttlPeriod())
+                    .mentioned(updates0.mentioned())
+                    .mediaUnread(updates0.mediaUnread())
+                    .silent(updates0.silent())
+                    .build();
+
+            update = ImmutableUpdateShort.builder()
+                    .date(updates0.date())
+                    .update(UpdateNewMessage.builder()
+                            .message(message)
+                            .pts(updates0.pts())
+                            .ptsCount(updates0.ptsCount())
+                            .build())
+                    .build();
+        } else if (updates instanceof UpdateShortChatMessage) {
+            UpdateShortChatMessage updates0 = (UpdateShortChatMessage) updates;
+            message = BaseMessage.builder()
+                    .silent(request.silent())
+                    .peerId(peer)
+                    .viaBotId(updates0.viaBotId())
+                    .replyTo(updates0.replyTo())
+                    .fwdFrom(updates0.fwdFrom())
+                    .message(request.message())
+                    .id(updates0.id())
+                    .replyMarkup(request.replyMarkup())
+                    .fromScheduled(request.scheduleDate() != null)
+                    .entities(updates0.entities())
+                    .date(updates0.date())
+                    .ttlPeriod(updates0.ttlPeriod())
+                    .mentioned(updates0.mentioned())
+                    .mediaUnread(updates0.mediaUnread())
+                    .silent(updates0.silent())
+                    .build();
+
+            update = ImmutableUpdateShort.builder()
+                    .date(updates0.date())
+                    .update(UpdateNewMessage.builder()
+                            .message(message)
+                            .pts(updates0.pts())
+                            .ptsCount(updates0.ptsCount())
+                            .build())
+                    .build();
+        } else if (updates instanceof BaseUpdates) {
+            BaseUpdates updates0 = (BaseUpdates) updates;
+
+            UpdateMessageID updateMessageID = updates0.updates().stream()
+                    .filter(updates1 -> updates1 instanceof UpdateMessageID)
+                    .map(updates1 -> (UpdateMessageID) updates1)
+                    .findFirst()
+                    .orElseThrow(IllegalStateException::new);
+
+            if (updateMessageID.randomId() != request.randomId()) {
+                throw new IllegalArgumentException("Incorrect random id. Excepted: " + request.randomId()
+                        + ", received: " + updateMessageID.randomId());
+            }
+
+            message = updates0.updates().stream()
+                    .filter(updates1 -> updates1 instanceof UpdateNewMessage)
+                    .map(updates1 -> (UpdateNewMessage) updates1)
+                    .filter(updates1 -> updates1.message() instanceof BaseMessage) // TODO: what is MessageService type??
+                    .map(updates1 -> (BaseMessage) updates1.message())
+                    .findFirst()
+                    .orElseThrow(IllegalStateException::new);
+
+            update = updates0;
+        } else {
+            throw new IllegalStateException("Unknown updates type: " + updates);
+        }
+
+        return Tuples.of(message, update);
+    }
+}
---
+++ mtproto/src/main/java/telegram4j/mtproto/service/RpcService.java
@@ -0,0 +1,60 @@
+package telegram4j.mtproto.service;
+
+import reactor.core.publisher.Mono;
+import telegram4j.mtproto.MTProtoClient;
+import telegram4j.mtproto.store.StoreLayout;
+import telegram4j.tl.*;
+
+import java.util.Objects;
+
+public abstract class RpcService {
+    protected final MTProtoClient client;
+    protected final StoreLayout storeLayout;
+
+    public RpcService(MTProtoClient client, StoreLayout storeLayout) {
+        this.client = Objects.requireNonNull(client, "client");
+        this.storeLayout = Objects.requireNonNull(storeLayout, "storeLayout");
+    }
+
+    public final MTProtoClient getClient() {
+        return client;
+    }
+
+    public final StoreLayout getStoreLayout() {
+        return storeLayout;
+    }
+
+    protected Mono<Peer> toPeer(InputPeer inputPeer) {
+        return Mono.defer(() -> {
+            switch (inputPeer.identifier()) {
+                case InputPeerEmpty.ID:
+                    return Mono.error(new IllegalArgumentException("Empty input peer: " + inputPeer));
+                case InputPeerSelf.ID:
+                    return storeLayout.getSelfId()
+                            .map(ImmutablePeerUser::of)
+                            .switchIfEmpty(Mono.error(new IllegalStateException(
+                                    "Failed to load self user id from store.")));
+                case InputPeerChannel.ID:
+                    InputPeerChannel inputPeerChannel = (InputPeerChannel) inputPeer;
+                    return Mono.just(ImmutablePeerChannel.of(inputPeerChannel.channelId()));
+                // TODO: test this
+                case InputPeerChannelFromMessage.ID:
+                    InputPeerChannelFromMessage inputPeerChannelFromMessage = (InputPeerChannelFromMessage) inputPeer;
+                    return Mono.just(ImmutablePeerChannel.of(inputPeerChannelFromMessage.channelId()));
+                case InputPeerChat.ID:
+                    InputPeerChat inputPeerChat = (InputPeerChat) inputPeer;
+                    return Mono.just(ImmutablePeerChat.of(inputPeerChat.chatId()));
+                case InputPeerUser.ID:
+                    InputPeerUser inputPeerUser = (InputPeerUser) inputPeer;
+                    return Mono.just(ImmutablePeerUser.of(inputPeerUser.userId()));
+                // TODO: test this
+                case InputPeerUserFromMessage.ID:
+                    InputPeerUserFromMessage inputPeerUserFromMessage = (InputPeerUserFromMessage) inputPeer;
+                    return Mono.just(ImmutablePeerUser.of(inputPeerUserFromMessage.userId()));
+                default:
+                    return Mono.error(new IllegalArgumentException("Unknown input peer type: 0x"
+                            + Integer.toHexString(inputPeer.identifier())));
+            }
+        });
+    }
+}
